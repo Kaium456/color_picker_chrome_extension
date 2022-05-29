5b0ae0c24bd6d34d7d5c508e4e832ba5281bb594
Kaium456 <kaiumbd456@gmail.com>
2022-05-29 07:21:47 +0600

    all code done

---
+++ popup.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+
+btn.addEventListener("click", async () => {
+  chrome.storage.sync.get("color", ({ color }) => {
+    console.log("color", color);
+  });
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (injectionResult) => {
+      const [data] = injectionResult;
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerHTML = color;
+
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      // console.log(injectionResult);
+    }
+  );
+});
+
+async function pickColor() {
+  try {
+    const eyeDropper = new EyeDropper();
+    return await eyeDropper.open();
+  } catch (err) {
+    console.error(err);
+  }
+}
